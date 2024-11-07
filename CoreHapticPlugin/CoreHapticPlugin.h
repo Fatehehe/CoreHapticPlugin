77788d780f1a893076541890c3e9f779384964e5
Fatih <fatakhillah.khaqo@gmail.com>
2024-11-07 09:28:57 +0700

    Initial Commit

---
+++ CoreHapticPlugin/CoreHapticPlugin.h
@@ -0,0 +1,18 @@
+//
+//  CoreHapticPlugin.h
+//  CoreHapticPlugin
+//
+//  Created by Fatakhillah Khaqo on 07/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreHapticPlugin.
+FOUNDATION_EXPORT double CoreHapticPluginVersionNumber;
+
+//! Project version string for CoreHapticPlugin.
+FOUNDATION_EXPORT const unsigned char CoreHapticPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreHapticPlugin/PublicHeader.h>
+
+
